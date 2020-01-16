04f43e1c84c044c07d0995315c7e1b1761126149
jaime-canales <jaime.canales@beetrack.com>
2020-01-16 12:51:07 -0300

    feat: include generic pagination

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,5 +1,18 @@
+# frozen_string_literal: true
+
 module Types
   class BaseObject < GraphQL::Schema::Object
     field_class Types::BaseField
+    # https://www.2n.pl/blog/graphql-pagination-in-rails
+    # https://graphql.org/learn/pagination/
+    def self.connection_type
+      connection_class = super
+      connection_class.define_method(:total_count) do
+        object.nodes.size
+      end
+
+      connection_class.send(:field, :total_count, Integer, null: false)
+      connection_class
+    end
   end
 end
